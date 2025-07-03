46ec790f233fbcf85ef5d8ad93f0d58960829395
SergioGBosco <sergiogibosco@gmail.com>
2025-07-03 18:38:18 +0200

    feat: [Bonus 2]  Insert variables with 5 or more characters into a new variable

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,7 +15,19 @@ console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+const longNames = [];
+
+for (let i = 0; i < teachers.length; i++) {
+  const longNamesteachers = teachers[i];
+
+  if (longNamesteachers.length >= 5) {
+    longNames.push(longNamesteachers)
+  }
+}
+
+console.log(teachers)
+console.log(longNames)
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
