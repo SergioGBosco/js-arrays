e6b64724aafc0bb8cb66cb18c9897304ad3fc20e
SergioGBosco <sergiogibosco@gmail.com>
2025-07-04 10:06:43 +0200

    fix [Bonus] use cicle for, for reverse  the order on array

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,7 +10,10 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
+const temparray = [];
+for (i = teachers.length - 1; i >= 0; i--)
+  temparray.push(teachers[i]);
+const reversedTeachers = temparray;
 console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
